7b225830983a72008b7971caf5674b9b621306ac
AryanIruma <115530462+IlhamKusumaAryanda@users.noreply.github.com>
2024-05-15 17:04:19 +0700

    Initial commit LFS
    UnityPrototype

---
+++ Assets/Script/AnimationStrings.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+    internal class AnimationStrings
+    {
+        internal static string isMoving = "isMoving";
+        internal static string isGrounded = "isGrounded";
+    }
